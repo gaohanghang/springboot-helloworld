2a4d7ba51d32bcb9e30daae6c98e468e4d7284f5
gaohanghang <1341947277@qq.com>
2018-03-23 15:07:17 +0800

    springboot

--- src/main/java/com/itmayiedu/app/App.java
+++ src/main/java/com/itmayiedu/app/App.java
@@ -7,14 +7,16 @@ import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan(basePackages = {"com.itmayiedu.controller","com.itmayiedu.service"})
-@EnableJpaRepositories("com.itmayiedu.dao")
+@ComponentScan(basePackages = { "com.itmayiedu.yml.controller" })
+// @EnableJpaRepositories("com.itmayiedu.dao")
 @EnableAutoConfiguration
-@EntityScan("com.itmayiedu.entity")
+// @EntityScan("com.itmayiedu.entity")
+
 public class App {
 
     public static void main(String[] args) {
-        //主函数运行springboot项目
+        // 主函数运行springboot项目
         SpringApplication.run(App.class, args);
     }
+
 }
